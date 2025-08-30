d320edb7afc1bc86471896315fe9a84994f4ee70
JiYoon Yang <yoon2fy@gmail.com>
2025-08-30 21:02:53 +0900

    Initial commit

---
+++ src/main/java/me/yoon2fy/findatabridge/FinDataBridgeApplication.java
@@ -0,0 +1,13 @@
+package me.yoon2fy.findatabridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinDataBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinDataBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/yoon2fy/findatabridge/FinDataBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yoon2fy.findatabridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinDataBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
